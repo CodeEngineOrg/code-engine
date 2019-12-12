bab6dfd6731b48ad7c6a35c07578cd429a694d97
James Messinger <james.messinger@gmail.com>
2019-12-12 06:20:27 -0600

    Updated to use @code-engine/cli

--- bin/code-engine.js
+++ bin/code-engine.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
 "use strict";
-const { main } = require("@code-engine/cli");
-main(process.argv.slice(2));
+
+const { CodeEngineCLI } = require("@code-engine/cli");
+
+let cli = new CodeEngineCLI();
+
+cli.main(process.argv.slice(2));
