1f81761d2d0fb151a50208f01b1bb1165429ff3a
James Messinger <james.messinger@gmail.com>
2020-01-11 06:12:11 -0600

    Pass the package.json manifest to to the CodeEngine CLI

--- bin/code-engine.js
+++ bin/code-engine.js
@@ -2,7 +2,7 @@
 "use strict";
 
 const { CodeEngineCLI } = require("@code-engine/cli");
+const manifest = require("../package.json");
 
-let cli = new CodeEngineCLI();
-
+let cli = new CodeEngineCLI({ manifest });
 cli.main(process.argv.slice(2));
