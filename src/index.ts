a91d95213ac078a07d5e6213f792b2b8f441fccf
James Messinger <james.messinger@gmail.com>
2020-07-20 07:00:06 -0500

    ESLint fixes

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,9 @@ export { Generator, WatchConfig } from "@code-engine/cli";
 export * from "@code-engine/types";
 
 // Export `CodeEngine` as the default export
-// tslint:disable: no-default-export
 export default CodeEngine;
 
 // CommonJS default export hack
 if (typeof module === "object" && typeof module.exports === "object") {
-  module.exports = Object.assign(module.exports.default, module.exports);  // tslint:disable-line: no-unsafe-any
+  module.exports = Object.assign(module.exports.default, module.exports);
 }
